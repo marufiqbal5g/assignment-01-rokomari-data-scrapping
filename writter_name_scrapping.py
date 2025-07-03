7519ee060ee043a9a93e3b23c5f3fedc6c45520e
KAZI MARUF IQBAL <88572657+marufiqbal5g@users.noreply.github.com>
2025-07-03 15:10:20 +0600

    Update writter_name_scrapping.py

--- writter_name_scrapping.py
+++ writter_name_scrapping.py
@@ -1,18 +1,31 @@
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
 
-driver.get('https://www.rokomari.com/book/authors?ref=sm_p0')
+# driver.get('https://www.rokomari.com/book/authors?ref=sm_p0')
+driver.get('https://www.rokomari.com/book/')
 
 driver.maximize_window()
 
+time.sleep(3)
+
+pop_up=driver.find_element(By.XPATH,'/html/body/div[3]/div[1]/button')
+time.sleep(2)
+pop_up.click()
+time.sleep(3)
+
+writter_subcatagory=driver.find_element(By.XPATH,'//*[@id="js--authors"]')
+action=ActionChains(driver)
+action.move_to_element(writter_subcatagory).click().perform()
+time.sleep(5)
+
 writter_name_list = []
 
-for page in range(1,61):
+for page in range(1,3):
     driver.get(f'https://www.rokomari.com/book/authors?ref=sm_p0&page={page}')
     for i in range(1,49):
         j = str(i)
@@ -24,8 +37,25 @@ for page in range(1,61):
 print(writter_name_list)
 print('Total Lengts of the writter names:',len(writter_name_list))
 
-file='C:\\D DRIVE\\ML_WITH_NLP_B_7\\ASSIGNMENT-01_DATA_SCRAPPING\\writters_name.txt'
 
-with open(file, 'w', encoding="utf-8") as f:
-    for item in writter_name_list:
-        f.write(f"{item}\n")
+
+# # Save writters name in txt file
+# file='C:\\D DRIVE\\ML_WITH_NLP_B_7\\ASSIGNMENT-01_DATA_SCRAPPING\\writters_name.txt'
+
+# with open(file, 'w', encoding="utf-8") as f:
+#     for item in writter_name_list:
+#         f.write(f"{item}\n")
+
+
+
+# Save writters name in excel file
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+
+# my_list = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+for index, item in enumerate(writter_name_list):
+    ws.cell(row=index + 1, column=1, value=item)
+
+wb.save("C:\\D DRIVE\\ML_WITH_NLP_B_7\\ASSIGNMENT-01_DATA_SCRAPPING\\Writters_Name.xlsx")
